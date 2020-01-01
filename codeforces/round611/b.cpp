b513962f91ef075d7c2932095a0b4ddda1d8bedd
Matthew Agar <mattagar6@gmail.com>
2019-12-31 22:00:39 -0500

    Create b.cpp

---
+++ codeforces/round611/b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define nav(...) "[ " << #__VA_ARGS__ ": " << (__VA_ARGS__) << " ] "
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef long long ll;
+template<class T> struct rge { T b, e; };
+template<class T> rge<T> range(T i, T j) { return rge<T>{i, j}; }
+struct debug {
+	~debug() {cout<<endl;}
+	template<class T> debug& operator<<(T x) {cout<<boolalpha<<x; return *this;}
+	template<class B, class C> debug& operator<< (pair<B, C> x){return *this<<"("<<x.first<<", "<<x.second<<")";}
+	template<class T> debug& operator<<(rge<T> x) {
+		*this<<"[";for(auto it=x.b;it!=x.e;it++){*this<<", "+2*(it==x.b)<<*it;}return *this<<"]";}
+	template<class T> debug& operator<<(vector<T> x){ return *this<<range(all(x));}
+};
+
+void solve(void) {
+
+	int n, k;
+	scanf("%d %d", &n, &k);
+	
+	int ans = n;
+	int extra = n % k;
+	if(extra > k/2) {
+		extra -= k/2;
+		ans -= extra;
+	}
+	printf("%d\n", ans);
+
+}
+
+int main(void) {
+
+	int tc;
+	scanf("%d", &tc);
+	for(int rep = 0; rep < tc; rep++)
+		solve();
+	
+	return 0;
+}
