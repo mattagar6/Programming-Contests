3c3d19f1bef21bf13529c531cd7c8a81e98fad71
Matthew Agar <mattagar6@gmail.com>
2020-03-17 19:06:42 -0400

    Delete test.sh

--- templates/test.sh
+++
@@ -1,7 +0,0 @@
-for((i = 1; ; ++i)); do
-    echo $i
-    ./gen.out $i > input.txt
-    ./sol.out < input.txt > out1.txt
-    ./brute.out < input.txt > out2.txt
-    diff -w out1.txt out2.txt || break
-done
