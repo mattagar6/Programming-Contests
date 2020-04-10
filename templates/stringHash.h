54f2ee2c41f0cb52624bab9eb36766753c34529a
Matthew Agar <mattagar6@gmail.com>
2020-04-10 14:27:21 -0400

    made prime powers global

--- templates/stringHash.h
+++ templates/stringHash.h
@@ -1,6 +1,9 @@
 const int MAXN = 100*1000+5;
 const int mod = 1000*1000*1000+7;
 
+vector<int> ppow[2], pinv[2];
+const int p[2] = {29, 31};
+
 int mul(int a, int b) { return 1LL * a * b % mod; }
 void fix(int& a) { while(a < 0) a += mod; while(a >= mod) a -= mod; }
 int add(int a, int b) { fix(a), fix(b), a += b; fix(a); return a; }
@@ -10,29 +13,29 @@ int mpow(int a, int n) {
     return x;
 }
 
+// call this to init prime powers and inverses
+void init() {
+    for(int i = 0; i < 2; i++) { ppow[i].resize(MAXN), pinv[i].resize(MAXN); } 
+    ppow[0][0] = ppow[1][0] = pinv[0][0] = pinv[1][0] = 1;
+    pinv[0][1] = mpow(p[0], mod-2), pinv[1][1] = mpow(p[1], mod-2);
+    for(int i = 0; i < 2; i++) { for(int j = 1; j < MAXN; j++) {
+        ppow[i][j] = mul(ppow[i][j/2], ppow[i][j/2]);
+        if(j&1) ppow[i][j] = mul(ppow[i][j], p[i]);
+        if(j==1) continue;
+        pinv[i][j] = mul(pinv[i][j/2], pinv[i][j/2]);
+        if(j&1) pinv[i][j] = mul(pinv[i][j], pinv[i][1]); } } }
+
 // assumes LOWERCASE latin letters
-template<int p1, int p2>
 class StringHash {
 public:
-    StringHash() { init(); }
+    StringHash() { memset(h, 0, sizeof h); }
     StringHash(string _s) {
         s = deque<char>(_s.begin(), _s.end());
         int n = s.size();
-        init();
+        memset(h, 0, sizeof h);
         for(int i = 0; i < 2; i++) for(int j = 0; j < n; j++) { h[i] = add(h[i], mul(_s[j]-'a'+1, ppow[i][j])); } }
 
-    void init() {
-        memset(h, 0, sizeof h);
-        for(int i = 0; i < 2; i++) { ppow[i].resize(MAXN), pinv[i].resize(MAXN); } 
-        ppow[0][0] = ppow[1][0] = pinv[0][0] = pinv[1][0] = 1;
-        pinv[0][1] = mpow(p[0], mod-2), pinv[1][1] = mpow(p[1], mod-2);
-        for(int i = 0; i < 2; i++) { for(int j = 1; j < MAXN; j++) {
-            ppow[i][j] = mul(ppow[i][j/2], ppow[i][j/2]);
-            if(j&1) ppow[i][j] = mul(ppow[i][j], p[i]);
-            if(j==1) continue;
-            pinv[i][j] = mul(pinv[i][j/2], pinv[i][j/2]);
-            if(j&1) pinv[i][j] = mul(pinv[i][j], pinv[i][1]); } } }
-
+    bool operator <(const StringHash& rhs) const { return tie(h[0], h[1]) < tie(rhs.h[0], rhs.h[1]); }
     bool operator ==(const StringHash& rhs) const { for(int i = 0; i < 2; i++) { if(h[i] != rhs.h[i]) return false; } return true; }
     bool operator !=(const StringHash& rhs) const { return !(*this == rhs); }
     size_t size() const { return s.size(); }
@@ -59,10 +62,8 @@ public:
         for(int i = 0; i < 2; i++) { h[i] = add(h[i], -(c-'a'+1)), h[i] = mul(h[i], pinv[i][1]); }
         s.pop_front(); }
 
+    pair<int, int> get_hash() const { return {h[0], h[1]}; }
 private:
     deque<char> s;
-    int p[2] = {p1, p2}, h[2];
-    vector<int> ppow[2], pinv[2];
+    int h[2];
 };
-
-using Hash = StringHash<29, 31>;
