83cc0d5b969f9e04dce84d490801251aa86d35d4
Matthew Agar <mattagar6@gmail.com>
2020-05-20 13:46:05 -0400

    changed implementation to Dinic's

--- templates/max_flow.cpp
+++ templates/max_flow.cpp
@@ -1,61 +1,83 @@
+// Dinic's algoritm for maximum flow O(E*V^2)
+// tested on: https://www.spoj.com/problems/FASTFLOW/
+
 template<typename flow_t>
-class MaxFlow {
+class Dinic {
 public:
 
-	MaxFlow(int _n, int _source, int _sink) : n(_n), source(_source), sink(_sink) {
-		g.resize(n);
-		flow.resize(n, vector<flow_t>(n));
-	}
+    Dinic(int _n, int _source, int _sink) : n(_n), source(_source), sink(_sink) {
+        g.resize(n);
+        flow.resize(n, vector<flow_t>(n));
+        level.resize(n);
+        ptr.resize(n);
+    }
 
-	void add_edge(int from, int to, flow_t cap) {
-		g[from].push_back(to);
-		g[to].push_back(from);
-		flow[from][to] += cap;
-	}
+    void add_edge(int from, int to, flow_t cap) {
+        g[from].push_back(to);
+        g[to].push_back(from);
+        flow[from][to] += cap;
+    }
 
-	flow_t get_flow() {
-		flow_t mf = 0;
+    bool bfs() {
+        vector<int> q{source};
+        for(int i = 0; i < (int) q.size(); i++) {
+            int u = q[i];
+            for(int v : g[u]) {
+                if(level[v] == -1 && flow[u][v] > 0) {
+                    level[v] = level[u] + 1;
+                    q.push_back(v);
+                }
+            }
+        }
+        
+        return level[sink] != -1;
+    }
+    
+    flow_t dfs(int u, flow_t pushed) {
+        if(pushed == 0) {
+            return 0;
+        }
+        if(u == sink) {
+            return pushed;
+        }
+        for(int& pos = ptr[u]; pos < (int) g[u].size(); pos++) {
+            int v = g[u][pos];
+            if(level[v] != level[u] + 1 || flow[u][v] == 0) {
+                continue;
+            }
+            flow_t maybe_push = dfs(v, min(pushed, flow[u][v]));
+            if(maybe_push == 0) {
+                continue;
+            }
+            flow[u][v] -= maybe_push;
+            flow[v][u] += maybe_push;
+            return maybe_push;
+        }
+        return 0;
+    }
 
-		vector<int> par(n), q;
-
-		while(true) {
-			par.assign(n, -1);
-			q.clear();
-			q.push_back(source);
-
-			for(int i = 0; i < (int) q.size(); i++) {
-				int u = q[i];
-				if(u == sink) break;
-				for(int v : g[u]) {
-					if(par[v] == -1 && flow[u][v] > 0) {
-						par[v] = u;
-						q.push_back(v);
-					}
-				}
-			}
-
-			if(par[sink] == -1) break;
-			flow_t f = FLOW_MAX;
-			int cur = sink;
-			while(cur != source) {
-				f = min(f, flow[par[cur]][cur]);
-				cur = par[cur];
-			}
-
-			mf += f;
-			cur = sink;
-			while(cur != source) {
-				flow[par[cur]][cur] -= f;
-				flow[cur][par[cur]] += f;
-				cur = par[cur];
-			}
-		}
-
-		return mf;
-	}
+    flow_t max_flow() {
+        flow_t mf(0);
+        while(true) {
+            level.assign(n, -1);
+            level[source] = 0;
+            if(!bfs()) {
+                break;
+            }
+            ptr.assign(n, 0);
+            flow_t now;
+            do {
+                now = dfs(source, FLOW_MAX);
+                mf += now;
+            } while(now != 0);
+        }
+        
+        return mf;
+    }
 private:
-	const flow_t FLOW_MAX = numeric_limits<flow_t>::max() / 2;
-	vector<vector<int>> g;
-	vector<vector<flow_t>> flow;
-	int n, source, sink;
+    const flow_t FLOW_MAX = numeric_limits<flow_t>::max() / 2;
+    vector<vector<int>> g;
+    vector<vector<flow_t>> flow;
+    vector<int> level, ptr;
+    int n, source, sink;
 };
