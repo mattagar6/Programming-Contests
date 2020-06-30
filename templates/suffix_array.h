979ae4314640b4c1e3415ee72d17c3b09283c4bd
Matthew Agar <mattagar6@gmail.com>
2020-06-30 13:32:33 -0400

    Update suffix_array.h

--- templates/suffix_array.h
+++ templates/suffix_array.h
@@ -22,10 +22,10 @@ namespace suffix_array {
       p.swap(new_p);
    }
    
-   // returns suffix array of given string "s" 
-   // appends a "$" to the end of "s", LENGTH++
+   // returns suffix array of given string s 
+   // appends a "$" to the end of s, LENGTH++
    // O(nlogn) time
-   vector<int> build(string& s) {
+   vector<int> build_sa(string& s) {
       s += "$";
       int n = s.length();
       vector<int> p(n), c(n);
@@ -68,5 +68,28 @@ namespace suffix_array {
          ++k;
       }
       return p;
+   }
+   
+   // returns lcp array from suffix array, stores suffix array in "p"
+   // lcp[i] = length of longest common prefix between strings "i" and "i-1" in suffix array
+   // indices are from [1, ..., n-1]
+   // O(n) time for "lcp", O(nlogn) time for "p"
+   vector<int> build_lcp(string& s, vector<int>& p) {
+      p = build_sa(s);
+      int n = s.length();
+      vector<int> lcp(n);
+      vector<int> c(n);
+      for(int i = 0; i < n; i++) {
+         c[p[i]] = i;
+      }
+      int k = 0;
+      for(int i = 0; i < n-1; i++) {
+         int pi = c[i];
+         int j = p[pi - 1];
+         while(s[i + k] == s[j + k]) ++k;
+         lcp[pi] = k;
+         k = max(k-1, 0);
+      }
+      return lcp;
    }
 } // namespace suffix_array
