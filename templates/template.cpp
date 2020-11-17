e358688557825b2a7f2586ec80d18d44f6c37ab2
Matthew Agar <mattagar6@gmail.com>
2020-11-17 16:35:39 -0500

    Add files via upload

---
+++ templates/template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define F first
+#define S second
+#define se second
+#define fi first
+#define pb push_back
+#define eb emplace_back
+#define mk make_pair
+
+#define REP(i,n) for(int i = 0; i < int(n); i++)
+#define FOR(i,a,b) for(int i = a; i <= int(b); i++)
+#define EACH(x,v) for(auto& x : v)
+
+#define N 1000007 //10e6 +7
+
+
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	
+}
