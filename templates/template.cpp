3c380156eeae6c8cb4e0181ee637e0fe7028b459
Matthew Agar <mattagar6@gmail.com>
2020-11-30 09:30:00 -0500

    Update template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,29 +1,30 @@
+//#pragma GCC optimize ("Ofast")  
+//#pragma GCC target ("avx,avx2,fma")
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long ll;
-typedef pair<int, int> pii;
+typedef long double ld;
+
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 
 #define F first
 #define S second
-#define se second
-#define fi first
-#define pb push_back
-#define eb emplace_back
 #define mk make_pair
+#define ar array
+#define all(x) x.begin(), x.end()
 
 #define REP(i,n) for(int i = 0; i < int(n); i++)
 #define FOR(i,a,b) for(int i = a; i <= int(b); i++)
-#define EACH(x,v) for(auto& x : v)
-
-#define N 1000007 //10e6 +7
 
 
 
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-
+	
 	
 }
