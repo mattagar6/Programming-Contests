d80815d73a7667cbc6b38ae68f2952f16a61359d
Matthew Agar <mattagar6@gmail.com>
2021-03-02 21:34:35 -0500

    changed to ICPC template

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,25 +1,57 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+ 
 using namespace std;
-
+ 
 typedef long long ll;
+typedef pair<int, int> pii;
 typedef long double ld;
-
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-
+ 
 #define F first
 #define S second
-#define ar array
-#define all(x) x.begin(), x.end()
+ 
+const int MOD = 1e9 + 7;//XXX
+const int C = 26;//XXX
+ 
+void add(int &x, int y){
+	x += y;
+	while (x >= MOD) x -= MOD;
+	while (x < 0) x += MOD;
+}
+ 
+int fix(int x){
+	while (x >= MOD) x -= MOD;
+	while (x < 0) x += MOD;
+	return x;
+}
+ 
+int pw(int a, int b){
+	int ret = 1;
+	while (b){
+		if (b & 1)
+			ret = 1ll*ret*a%MOD;
+		b >>= 1;
+		a = 1ll*a*a%MOD;
+	}
+	return ret;
+}
+ 
+const int MAXN = 1e6 + 10;
 
-#define REP(i,n) for(int i = 0; i < int(n); i++)
-#define FOR(i,a,b) for(int i = a; i <= int(b); i++)
+void solve() {
+	
+}
 
-
-
-int main() {
-	ios::sync_with_stdio(0);
+int main(){
+	ios::sync_with_stdio(false);
 	cin.tie(0);
+	cout.tie(0);
+	//cout << fixed << setprecision(6);
 	
-	
+	int te = 1;	
+	//cin >> te;
+	for (int w = 1; w <= te; w++){
+		//cout << "Case #" << w << ": ";
+		solve();
+	}
+	return 0;
 }
